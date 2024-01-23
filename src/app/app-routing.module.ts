f762bce83800c17dd92e5be03f5e38022cbe5447
Jaime <jaime99sq@gmail.com>
2024-01-23 20:41:02 +0100

    instalacion tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginRoutedComponent } from './components/shared/login-routed/login-routed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginRoutedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

27dc6c5d1b4c0935e54fe6b90f6005273ea3ad73
Jaime <jaime99sq@gmail.com>
2024-05-19 21:07:40 +0200

    Responsividad carrito y mejoras pdf

--- src/app/service/compra.print.service.ts
+++ src/app/service/compra.print.service.ts
@@ -30,24 +30,22 @@ export class CompraPrintService {
     sp = (n: number): string => n.toLocaleString('es', { minimumFractionDigits: 2, maximumFractionDigits: 2});
 
     printFacturaCompra = (id_compra: number): void => {
-       this.compraAjaxService.getCompraById(id_compra).subscribe({
+        this.compraAjaxService.getCompraById(id_compra).subscribe({
             next: (compra: ICompra) => {
                 this.detalleCompraAjaxService.getDetalleCompraPageByCompraId(id_compra, 10, 0, 'id', 'asc').subscribe({
                     next: (detallesCompraPage: IDetalleCompraPage) => {
                         const detallesCompra = detallesCompraPage.content;
-
                         var doc = new jsPDF();
-                        doc.setFont('Courier');
+                        doc.setFont('Montserrat-VariableFont_wght', 'normal');
                         var imgData: string = '/assets/img/Captura de pantalla 2024-01-26 a las 12.35.57.png'
                         this.loadImage(imgData).then((logo) => {
                             doc = this.cabecera(doc, compra, logo);
                             doc.setFontSize(12);
                             var linea = 155;
                             let totalFactura = 0;
-                            doc.setFont('Courier');
                             detallesCompra.forEach((detalleCompra, index) => {
                                 this.lineaFactura(doc, detalleCompra, linea);
-                                linea += 7;
+                                linea += 12;  // Increased space between lines
                                 if (linea > 230 && index + 1 < detallesCompra.length) {
                                     doc.addPage();
                                     doc = this.cabecera(doc, compra, logo);
@@ -70,57 +68,46 @@ export class CompraPrintService {
             error: (error) => {
                 console.error("Ha habido un error al obtener la compra:", error);
             }
-       });
+        });
     }
 
     private cabecera(doc: any, compra2Print: ICompra, logo: any): any {
         const baseX = 10;
-        doc.setFont('bold');
         doc.setFontSize(20);
         doc.text('Factura', 90, 30);
-        doc.setFillColor(210, 210, 210);
-        doc.rect(baseX, 35, 105, 35, 'F');
+
         doc.addImage(logo, 'PNG', 20, 40, 80, 25);
 
-        doc.setFillColor(210, 210, 210);
-        doc.rect(120, 35, 80, 15, 'F');
+        // Apply colors from the specified palette
+        doc.setTextColor(63, 53, 122);  // Using governor-bay 900
         doc.setFontSize(12);
-        doc.text(130, 44, `Número de Factura: ${compra2Print.codigoPedido}`);
+        doc.text(`Número de Factura: ${compra2Print.codigoPedido}`, 130, 44);
+        doc.text('Fecha: ' + formatDate(compra2Print.fecha, 'dd/MM/yyyy', 'es-ES'), 130, 64);
 
-        doc.setFillColor(210, 210, 210);
-        doc.rect(120, 55, 80, 15, 'F');
-        doc.setFontSize(12);
-        doc.text(130, 64, 'Fecha: ' + formatDate(compra2Print.fecha, 'dd/MM/yyyy', 'es-ES'));
-
-        doc.setFillColor(210, 210, 210);
-        doc.rect(baseX, 75, 190, 50, 'F');
-
-        const clienteX = 25;
-        const clienteY = 85;
-
+        // Client information
         doc.setFontSize(14)
-        doc.text('Cliente: ', clienteX - 10, clienteY)
+        doc.text('Cliente: ', 15, 85)
         doc.setFontSize(15)
-        const cliente = compra2Print?.usuario?.nombre + ' ' + compra2Print?.usuario?.apellido + ' ' +  `${compra2Print?.usuario?.username}`;
-        doc.text(cliente, clienteX, clienteY + 10)
-        doc.setFont('normal');
+        const cliente = compra2Print?.usuario?.nombre + ' ' + compra2Print?.usuario?.apellido + ' ' + `${compra2Print?.usuario?.username}`;
+        doc.text(cliente, 25, 95)
         doc.setFontSize(14)
-        doc.text(compra2Print?.usuario?.direccion, clienteX, clienteY + 20)
-        doc.text(compra2Print?.usuario?.email, clienteX, clienteY + 30)
+        doc.text(compra2Print?.usuario?.direccion, 25, 105)
+        doc.text(compra2Print?.usuario?.email, 25, 115)
 
-        const emisorX = 140;
-        const emisorY = 85;
+        // Issuer information
         doc.setFontSize(14)
-        doc.text('Emitida por: ', emisorX - 10, emisorY)
+        doc.text('Emitida por: ', 130, 85)
         doc.setFontSize(18);
-        doc.text('90:24', emisorX, emisorY + 10);
+        doc.text('90:24', 140, 95);
         doc.setFontSize(12);
-        doc.text('noventaveinticuatro@gmail.com', emisorX, emisorY + 20)
+        doc.text('noventaveinticuatro@gmail.com', 140, 105)
         doc.setFontSize(10);
-        doc.text('C/ Ronaldo Messi Bryant 114', emisorX, emisorY + 30)
+        doc.text('C/ Ronaldo Messi Bryant 114', 140, 115)
 
         doc.line(15, 130, 195, 130);
 
+        // Table headers
+        doc.setTextColor(31, 81, 81);  // Using tradewind 900
         doc.text('Producto', 30, 140);
         doc.text('Talla', 80, 140);
         doc.text('Manga', 100, 140);
@@ -132,13 +119,14 @@ export class CompraPrintService {
         doc.line(15, 145, 195, 145);
 
         return doc;
-
     }
 
     private lineaFactura(doc: any, detalleCompra: IDetalleCompra, linea: number): void {
 
+        doc.setFillColor(242, 251, 250);
+        doc.rect(10, linea - 3,  190, 10, 'F');
+
         doc.setFontSize(8);
-        // doc.text(detalleCompra.camiseta.titulo, 15, linea);
         const titulo = detalleCompra.camiseta.titulo;
         const maxLength = 30;
         if (titulo.length > maxLength) {
@@ -146,9 +134,12 @@ export class CompraPrintService {
             const secondLine = titulo.substring(maxLength);
             doc.text(firstLine, 15, linea);
             doc.text(secondLine, 15, linea + 5);
-            doc.text('', 15, linea + 20);
+            // Add an extra blank line
+            doc.text('', 15, linea + 10);
         } else {
             doc.text(titulo, 15, linea);
+            // Add an extra blank line
+            doc.text('', 15, linea + 5);
         }
 
         const manga = detalleCompra.camiseta.manga === "Sin Mangas" ? "Sin" : detalleCompra.camiseta.manga;
@@ -160,7 +151,6 @@ export class CompraPrintService {
         doc.text(this.sp(detalleCompra.camiseta.precio), 156, linea, 'right');
         doc.text(this.sp(detalleCompra.iva), 173, linea, 'right');
         doc.text(this.sp(detalleCompra.cantidad * detalleCompra.camiseta.precio), 194, linea, 'right');
-
     }
 
     endFactura(doc: any, linea: number, totalFactura: number, compra2Print: ICompra): void {

dc3549c039761e1b49a3ad72dc937b742d445884
Jaime Serrano Quílez <jaime.serrano@viewnext.com>
2024-06-03 17:24:31 +0200

    Añado componente registro (falta por implementar)

---
+++ src/app/components/shared/register-routed/register-routed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-routed',
+  templateUrl: './register-routed.component.html',
+  styleUrls: ['./register-routed.component.css']
+})
+export class RegisterRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
